899f9a71a02e961fd56e78d47fc8ba8684a75f56
prashant-maker769 <76271643+prashant-maker769@users.noreply.github.com>
2024-03-29 09:42:08 +0530

    Update mac-changer.py

--- mac-changer.py
+++ mac-changer.py
@@ -1,6 +1,18 @@
 import subprocess
 # subprocess module allow us to run linux commands in python
+import optparse
+#optparse package helps to pass command line arguments
 
+# 1-Passing linux commands to python code to change mac-address
+"""
+subprocess.call(["ifconfig","eth0","down"])
+subprocess.call(["ifconfig","eth0","hw","ether","00:11:22:33:44:55"])
+subprocess.call(["ifconfig","eth0","up"])
+"""
+
+# 2-Introducing varibales in subprocess module
+# variables 
+interface = 
 subprocess.call(["ifconfig","eth0","down"])
 subprocess.call(["ifconfig","eth0","hw","ether","00:11:22:33:44:55"])
 subprocess.call(["ifconfig","eth0","up"])
