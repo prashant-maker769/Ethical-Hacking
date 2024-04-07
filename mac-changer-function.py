b77839c2adf97bde17f119879f6583b751e06cdb
prashant-maker769 <76271643+prashant-maker769@users.noreply.github.com>
2024-04-07 10:29:42 +0530

    regex mac-changer-function.py

--- mac-changer-function.py
+++ mac-changer-function.py
@@ -1,7 +1,9 @@
-import subprocess
 # subprocess module allow us to run linux commands in python
-import optparse
+import subprocess
 #optparse package helps to pass command line arguments
+import optparse
+#importing regular expression
+import re
 
 
 
@@ -19,5 +21,24 @@ def change_mac_address(user_interface,user_mac_address):
     subprocess.call(["ifconfig",user_interface,"hw","ether",user_mac_address])
     subprocess.call(["ifconfig",user_interface,"up"])
 
+# saving subprocess 
+def control_new_mac(interface):
+    ifconfig = subprocess.check_output(["ifconfig",interface])
+    
+# To check the mac_address has been changed or not for that we need to extract the mac-address of interface before and after code execution
+# For that REGEX (reglular expression) will help us, it is used to match the strings of text such as particular words, characters, pattern, etc.
+    #\w means any word character as we have 12 charcater for mac-address
+    new_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig)
+    if new_mac:
+        return new_mac.group(0)
+    else:
+        return None
+
 print("Mac Changer Started!")
+(user_input, arguments) = get_user_input()
 change_mac_address(user_input.interface,user_input.mac_address)
+finalized_mac = control_new_mac(user_input.interface):
+if finalized_,ac == user_input.mac_address:
+    print("Success!")
+else:
+    print("error?????????")
